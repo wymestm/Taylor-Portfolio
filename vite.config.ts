793b59f68b85af538dba208d4722e79aaf54075b
Taylor Wymes <wymestm@gmail.com>
2026-03-01 02:00:57 -0500

    updated vite base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/Taylor-Portfolio/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
